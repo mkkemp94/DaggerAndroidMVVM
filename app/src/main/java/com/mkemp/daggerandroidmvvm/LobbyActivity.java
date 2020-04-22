7c6470dd1f2bec7a9886bcefe31392ff6b6e6547
MKemp <mkemp@accu-time.com>
2020-04-22 09:49:06 -0400

    Initial commit

---
+++ app/src/main/java/com/mkemp/daggerandroidmvvm/LobbyActivity.java
@@ -0,0 +1,16 @@
+package com.mkemp.daggerandroidmvvm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LobbyActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lobby_activity);
+    }
+}
